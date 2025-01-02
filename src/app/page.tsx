d45427663322eda24ea227faf000a5e9e56ae756
xndroli <103154250+xndroli@users.noreply.github.com>
2025-01-02 07:17:23 -0500

    i and setup @clerk/nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
-    <h1 className="text-red-600">Hello world</h1>
+    <Button>Click Me</Button>
   );
 };
